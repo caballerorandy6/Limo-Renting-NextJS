f25f15cef323c4ee66e5a3caf00604dc3022fa3a
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2024-10-22 23:54:39 -0500

    deleted page in Info

--- src/app/info/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-const Info = () => {
-  // Redirige al usuario a la página principal
-  redirect("/");
-  return null; // No se renderiza nada, porque ya se está redirigiendo
-};
-
-export default Info;
